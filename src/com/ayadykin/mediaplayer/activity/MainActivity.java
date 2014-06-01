ee7f01da5fa4c0d016ca25d43dda8bbdf3fba69e
Andrei <mikolaev-andrei@ukr.net>
2014-06-01 20:16:19 +0300

    test

--- src/com/ayadykin/mediaplayer/activity/MainActivity.java
+++ src/com/ayadykin/mediaplayer/activity/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		//Git test
 		// Choose source for activity start
 		PreferenceUtils utils = new PreferenceUtils(this);
 		Bundle args = new Bundle();
